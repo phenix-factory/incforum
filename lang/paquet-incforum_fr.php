3b616ee8adf2a6c6a57613c9fe459316897d48c9
Debondt Didier <p@henix.be>
2016-08-24 15:31:23 +0200

    Init

---
+++ lang/paquet-incforum_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// I
+	'incforum_description' => '',
+	'incforum_nom' => 'Inclure pour Forum',
+	'incforum_slogan' => 'Des inclures sain pour les forums SPIP',
+);
